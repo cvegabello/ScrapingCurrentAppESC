728c9539eb015c967d2f4aa5723e4e5fde1f8160
cvegabello <carlosvegabello@gmail.com>
2021-06-05 06:28:57 -0400

    Database access and create file flag

---
+++ dataBaseMySQLClass.py
@@ -0,0 +1,37 @@
+import pymysql
+from pymysql import Error
+
+class DataBase:
+    def __init__(self) -> None:
+        try:
+            self.connection = pymysql.connect(
+                host = '10.5.165.52',
+                port = 3306,
+                user= 'opsNY',
+                password= '',
+                db= 'boardinfony'
+            )
+            self.cursor = self.connection.cursor()
+            print("connection successful")
+        except Error as ex:
+            print("Connection error:", ex)
+    
+    def select_returnOneRecord(self, sqlStr):
+        try:
+            self.cursor.execute(sqlStr)
+            record = self.cursor.fetchone()
+            return record
+
+        except Exception as e:
+            raise
+
+    def queryNoRecords(self, sqlStr):
+        try:
+            self.cursor.execute(sqlStr)
+            self.connection.commit()
+        except Exception as e:
+            raise
+
+    def close(self):
+        self.connection.close()
+        
--- extractCurrentApp.py
+++ extractCurrentApp.py
@@ -1,103 +1,89 @@
+from dataBaseMySQLClass import DataBase
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 #import pandas as pd
 import time
 import pymysql
-# import mysql.connector
 from pymysql import Error
+from datetime import datetime
 
 
-# # definer pagina a scrapear y ruta donde descargaste chromediver
-# website = 'http://nypdcweb/escadmin/Login.do'
-# path = 'C:/Users/cvegabello/Documents/VisualStudio2008/Projects/scrapingCurrentAppESC/chromedriver' #escribe tu ruta aqui
+# definer pagina a scrapear y ruta donde descargaste chromediver
+website = 'http://nypdcweb/escadmin/Login.do'
+path = './chromedriver' #escribe tu ruta aqui
 
-# # definer variable 'driver'
-# driver = webdriver.Chrome(path)
-# # abrir Google Chrome mediante chromedriver
-# driver.get(website)
-# driver.maximize_window()
-# #driver.fullscreen_window()
+# definer variable 'driver'
+driver = webdriver.Chrome(path)
+# abrir Google Chrome mediante chromedriver
+driver.get(website)
+driver.maximize_window()
 
-# time.sleep(3)
+time.sleep(3)
 
-# userNameTx = driver.find_element_by_name("username")
-# userNameTx.send_keys("cvega")
+userNameTx = driver.find_element_by_name("username")
+userNameTx.send_keys("cvega")
 
-# passwordTx = driver.find_element_by_name("password")
-# passwordTx.send_keys("OpsrEsc")
+passwordTx = driver.find_element_by_name("password")
+passwordTx.send_keys("OpsrEsc")
 
-# signInBtn = driver.find_element_by_name("submit")
-# signInBtn.click()
+signInBtn = driver.find_element_by_name("submit")
+signInBtn.click()
 
-# time.sleep(3)
+time.sleep(3)
 
-# appMenu = driver.find_element_by_xpath('//*[@id="gtech-scroll-header"]/table/tbody/tr/td/div/ul/table/tbody/tr/td[4]/li/a')
-# appMenu.click()
+appMenu = driver.find_element_by_xpath('//*[@id="gtech-scroll-header"]/table/tbody/tr/td/div/ul/table/tbody/tr/td[4]/li/a')
+appMenu.click()
 
-# currentAppMenu = driver.find_element_by_link_text ('Current Applications')
-# currentAppMenu.click()
+currentAppMenu = driver.find_element_by_link_text ('Current Applications')
+currentAppMenu.click()
 
-# driver.fullscreen_window()
-# # appsList = driver.find_elements_by_xpath("//tbody/tr[@role='row']")
+driver.fullscreen_window()
 
-# #appsNameList = driver.find_elements_by_tag_name("u")
-# appsNameList = driver.find_elements_by_xpath("//tbody/tr[@role='row']/td[2]")
-# appsVersionList = driver.find_elements_by_xpath("//tbody/tr[@role='row']/td[4]")
-# appsLastSubmitList = driver.find_elements_by_xpath("//tbody/tr[@role='row']/td[5]")
+appsNameList = driver.find_elements_by_xpath("//tbody/tr[@role='row']/td[2]")
+appsVersionList = driver.find_elements_by_xpath("//tbody/tr[@role='row']/td[4]")
+appsLastSubmitList = driver.find_elements_by_xpath("//tbody/tr[@role='row']/td[5]")
+
+driver.close
+
+appsNameListStr = []
+for app in appsNameList:
+    appsNameListStr.append(app.text)
+
+appsLongVersionListStr = []
+for app in appsVersionList:
+    appsLongVersionListStr.append(app.text)
 
 
-# driver.close
+appsLastSubmitListStr = []
+for app in appsLastSubmitList:
+    fecha_dt = datetime.strptime(app.text, '%b %d, %Y %I:%M:%S %p')
+    appsLastSubmitListStr.append(fecha_dt)
 
-# appsNameListStr = []
-# for app in appsNameList:
-#     appsNameListStr.append(app.text)
+numElements = len(appsNameList)
 
-# # for element in appsNameListStr:
-# #     print(element)
+appsShortVersionListStr = []
+for i in range(numElements):
+    posFirstInt = appsLongVersionListStr[i].find(":")
+    posSegInt = appsLongVersionListStr[i].find(":", posFirstInt + 1)
+    appsShortVersionListStr.append(appsLongVersionListStr[i][posSegInt + 1:])
 
-# appsLongVersionListStr = []
-# for app in appsVersionList:
-#     appsLongVersionListStr.append(app.text)
-
-# # for element in appsLongVersionListStr:
-# #     print(element)
-
-# appsLastSubmitListStr = []
-# for app in appsLastSubmitList:
-#     appsLastSubmitListStr.append(app.text)
-
-# # for element in appsLastSubmitListStr:
-# #     print(element)
-
-# numElements = len(appsNameList)
-# # for i in range(numElements):
-# #     print(appsNameListStr[i] + "|" + appsLongVersionListStr[i] + "|" + appsLastSubmitListStr[i] )
-
-# appsShortVersionListStr = []
-# for i in range(numElements):
-#     posFirstInt = appsLongVersionListStr[i].find(":")
-#     posSegInt = appsLongVersionListStr[i].find(":", posFirstInt + 1)
-#     appsShortVersionListStr.append(appsLongVersionListStr[i][posSegInt + 1:])
-
-# # for element in appsShortVersionListStr:
-# #     print(element)   
-
-# for i in range(numElements):
-#     print(appsNameListStr[i] + "|" + appsShortVersionListStr[i] + "|" + appsLastSubmitListStr[i] )
+driver.quit()
 
 #Database
-#host = '10.5.165.52',
-try:
-    connection = pymysql.connect(
-        host = '10.5.165.52',
-        port = 3306,
-        user= 'opsNY',
-        password= '',
-        db= 'boardinfony'
-    )
-    print("connection successful")
-except Error as ex:
-    print("Connection error:", ex)
+database = DataBase()
+sqlStr = "DELETE FROM terminal_application"
+database.queryNoRecords(sqlStr)
+
+for i in range(numElements):
+    sqlStr = "INSERT INTO boardinfony.terminal_application (application_name, version, update_datetime) VALUES ('{}', '{}', '{}')".format(appsNameListStr[i], appsShortVersionListStr[i], appsLastSubmitListStr[i])
+    database.queryNoRecords(sqlStr)
+
+database.close()
+
+# Create File Flag
+with open("//10.5.165.84/SharedFolderBoard/flagFileAppVersion.txt", "w" , encoding="utf-8") as f:
+    f.write(str(datetime.now) + ": Ready Version Terminal")
+    f.close
 
 
 # Pandas
